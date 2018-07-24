833cccb1ca01b1dacda6f9f9c93952c330afca89
divaweb <lanittad@hotmail.com>
2018-07-24 08:40:24 -0600

    fixing bootstrap javascrpt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-@import "bootstrap-sprockets";
-@import "bootstrap";
