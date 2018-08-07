88b2c824d12d082cd3665d701a34e300ffa1f9c6
divaweb <lanittad@hotmail.com>
2018-08-07 08:37:35 -0600

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_08_06_030459) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string "title"
-    t.text "copy"
-    t.integer "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id"
