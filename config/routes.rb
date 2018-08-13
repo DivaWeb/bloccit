40b50c282b1c8a25a483705ef5e63cbb8264db70
divaweb <lanittad@hotmail.com>
2018-08-13 08:16:45 -0600

    signin checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
    resources :users, only: [:new, :create, :show]
    resources :users, only: [:new, :create]
    post 'users/confirm' => 'users#confirm'
+   resources :sessions, only: [:new, :create, :destroy]
 
 
 
